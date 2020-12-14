4dd0b0eb9ff56fd9e707b5e3e02bef8b32277f58
Vadim-Nikitenko <nikagw24@gmail.com>
2020-12-14 11:50:12 +0300

    Lesson7

--- app/src/main/java/ru/geekbrains/poplib/di/modules/ImageModule.kt
+++ app/src/main/java/ru/geekbrains/poplib/di/modules/ImageModule.kt
@@ -7,10 +7,12 @@ import ru.geekbrains.poplib.mvp.model.cache.IImageCache
 import ru.geekbrains.poplib.mvp.model.image.IImageLoader
 import ru.geekbrains.poplib.mvp.model.network.INetworkStatus
 import ru.geekbrains.poplib.ui.image.GlideImageLoader
+import javax.inject.Singleton
 
 @Module
 class ImageModule {
 
+    @Singleton
     @Provides
     fun  glideImageLoader(imageCache: IImageCache, networkStatus: INetworkStatus): IImageLoader<ImageView> = GlideImageLoader(imageCache, networkStatus)
 }
